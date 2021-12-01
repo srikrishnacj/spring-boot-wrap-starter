3ea90fae9b8f29520a393a6335daaa684d884e19
Cj <srikrishna.cj@gmail.com>
2021-12-01 12:08:56 +0530

    bom module introduced

---
+++ spring-boot-wrap-starter-bom/src/test/java/in/cjcj/sboa/AppTest.java
@@ -0,0 +1,20 @@
+package in.cjcj.sboa;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
